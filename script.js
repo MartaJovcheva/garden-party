86973140049cc8e9ef816a79cd6651625ee30fcc
Marta Jovcheva <159816726+MartaJovcheva@users.noreply.github.com>
2024-08-06 22:32:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showGarden() {
     document.getElementById('invitation').style.display = 'none'; // Hide the invitation image
-    document.getElementById('gardenContent').style.display = 'block'; // Show the garden image
+    document.getElementById('gardenContent').style.display = 'flex'; // Show the garden image
 }
